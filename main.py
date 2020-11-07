a01dc5c0bda851d740279432f77b96320e3ae934
OliMar655 <oliverfma2065@outlook.com>
2020-11-07 14:04:02 -0500

    Added stuff

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ root = tk.Tk()
 apps = []
 
 def addApp():
+
+    for widget in frame.winfo_children():
+        widget.destroy()
+
     filename = filedialog.askopenfilename(initialdir="/", title="Select File", filetypes=(("executables", "*.pdf"),("all files", "*.*")))
 
-    app.append(filename)
+    apps.append(filename)
     print(filename)
     for app in apps:
-        label = tk.Label(frame, text=app)
+        label = tk.Label(frame, text=app, bg ="gray")
         label.pack()
+
+def runApp():
+    for app in apps:
+        os.startfile(app)
 
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
 canvas.pack() #adds green background
@@ -24,7 +32,7 @@ frame.place(relwidth=0.8, relheight=0.8, relx = 0.1, rely = 0.1) # centers white
 openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#263D42", command=addApp)
 openFile.pack()
 
-runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#263D42")
+runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#263D42", command=runApp)
 runApps.pack()
 
 

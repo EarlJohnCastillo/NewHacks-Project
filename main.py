4ba3e8c637f5d52d859e210abe38dbcd0f1da679
OliMar655 <oliverfma2065@outlook.com>
2020-11-07 13:47:42 -0500

    Added stuff

--- main.py
+++ main.py
@@ -5,19 +5,15 @@ import os
 
 root = tk.Tk()
 
+
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
 canvas.pack() #adds green background
 
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx = 0.1, rely = 0.1) # centers white box
 
-openFile = tk.Button(root, text=|import tkinter as tk
-from tkinter import filedialog, Text
-import os
-
-root = tk.Tk()
-
-root.mainloop("Open File", padx=10, pady=5, fg="white", bg ="#263D42)
+openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#263D42")
+openFile.pack()
 
 
 root.mainloop()
